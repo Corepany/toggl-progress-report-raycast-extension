d5349fd672a66152353a014b2e7b36cedd4f4c4c
Corepany <cemalsert@gmail.com>
2023-03-15 22:23:20 +0300

    Refactor

--- src/components/ReportItem.tsx
+++ src/components/ReportItem.tsx
@@ -1,5 +1,5 @@
 import { List, ActionPanel, Action, Icon, getPreferenceValues } from "@raycast/api";
-import { ReportItemProps } from "../types";
+import { ReportItemProps, TogglReport } from "../types";
 import { getProgressIcon } from "@raycast/utils";
 import ReportDetail from "./ReportDetail";
 
@@ -9,20 +9,33 @@ function formatTime(totalTime: number): string {
   return `${hours}h ${minutes}m`;
 }
 
+function calculateProgressData(report: TogglReport, reportType: string) {
+  let progress = 0;
+  let remainingTime = 0;
+  let remainingTimeText = "";
+
+  if (reportType === "weekly") {
+    progress = (report.totalTime || 0) / report.weeklyGoal;
+    remainingTime =
+      progress > 1 ? (report.weeklyGoal - report.totalTime) * -1 : (report.totalTime - report.weeklyGoal) * -1;
+    remainingTimeText =
+      progress > 1 ? `Overworked time: ${formatTime(remainingTime)}` : `Remaining time: ${formatTime(remainingTime)}`;
+  } else if (reportType === "monthly") {
+    progress = (report.totalTime || 0) / report.monthlyGoal;
+    remainingTime =
+      progress > 1 ? (report.monthlyGoal - report.totalTime) * -1 : (report.totalTime - report.monthlyGoal) * -1;
+    remainingTimeText =
+      progress > 1 ? `Overworked time: ${formatTime(remainingTime)}` : `Remaining time: ${formatTime(remainingTime)}`;
+  }
+
+  return { progress, remainingTimeText };
+}
+
 export function ReportItem(props: ReportItemProps) {
   const { report, onGoalUpdate } = props;
   const reportType = getPreferenceValues().reportType;
+  const { progress, remainingTimeText } = calculateProgressData(report, reportType);
 
-  let progress = 0;
-  let remainingTime = 0;
-
-  if (reportType == "weekly") {
-    progress = (report.totalTime || 0) / report.weeklyGoal;
-    remainingTime = (report.totalTime - report.weeklyGoal) * -1;
-  } else if (reportType == "monthly") {
-    progress = (report.totalTime || 0) / report.monthlyGoal;
-    remainingTime = (report.totalTime - report.monthlyGoal) * -1;
-  }
   return (
     <List.Item
       id={report.id.toString()}
@@ -30,7 +43,7 @@ export function ReportItem(props: ReportItemProps) {
       icon={getProgressIcon(progress)}
       accessories={[
         {
-          text: `Total time: ${formatTime(report.totalTime || 0)} / Remaining time: ${formatTime(remainingTime)}`,
+          text: `Total time: ${formatTime(report.totalTime || 0)} / ${remainingTimeText}`,
         },
       ]}
       actions={
